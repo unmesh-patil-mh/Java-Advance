8aec6de4f0caee61d166c3d493091e4bcba89987
Unmesh Patil <punmesh56@gmail.com>
2026-01-27 22:35:09 +0530

    Add files via upload

---
+++ GenericServlet/src/main/java/GenericServletEx/GenericSer.java
@@ -0,0 +1,22 @@
+package GenericServletEx;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/Servlet1")
+public class GenericSer extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		PrintWriter out=res.getWriter();
+		String name=req.getParameter("uname");
+		out.print("<h1>Hello "+name+"</h1>");
+		
+	}
+}
